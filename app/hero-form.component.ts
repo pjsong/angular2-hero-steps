a40c9cf93d830012f1c9c831d6749628eb3a88fd
jeson_peng <jeson.peng@gmail.com>
2016-09-05 23:27:10 +0800

    step3: add form, reset form and submit form

--- app/hero-form.component.ts
+++ app/hero-form.component.ts
@@ -12,4 +12,12 @@ export class HeroFormComponent {
   onSubmit() { this.submitted = true; }
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
+
+  active = true;
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
+  }
 }
