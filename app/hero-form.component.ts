aa5e79a85498e2a6642f127f4f9cdc922f00ca55
jeson_peng <jeson.peng@gmail.com>
2016-09-06 01:05:30 +0800

    continue with https://angular.io/docs/ts/latest/cookbook/form-validation.html

--- app/hero-form.component.ts
+++ app/hero-form.component.ts
@@ -5,8 +5,7 @@ import { Hero }    from './hero';
   templateUrl: 'app/hero-form.component.html'
 })
 export class HeroFormComponent {
-  powers = ['Really Smart', 'Super Flexible',
-    'Super Hot', 'Weather Changer'];
+  powers = ['Really Smart', 'Super Flexible',  'Super Hot', 'Weather Changer'];
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
   submitted = false;
   onSubmit() { this.submitted = true; }
