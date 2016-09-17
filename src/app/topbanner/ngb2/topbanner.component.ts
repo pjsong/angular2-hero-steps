89185a6e75c11a10e8fc72a29e1f1c2307c73955
jeson_peng <jeson.peng@gmail.com>
2016-09-18 00:12:35 +0800

    subject:remove click-down on homepage, fix click-down position use pip, ref:https://angular.io/docs/ts/latest/api/common/index/DecimalPipe-pipe.html;voidcanvas.com/angular-2-pipes-filters/ todo:materilize homepage

--- src/app/topbanner/ngb2/topbanner.component.ts
+++ src/app/topbanner/ngb2/topbanner.component.ts
@@ -1,7 +1,9 @@
+// src/app/topbanner/ngb2/topbanner.component.ts
 import {Component, OnInit, OnDestroy} from '@angular/core';
 
 // webpack html imports
 let template = require('./topbanner.component.html');
+let waitingCnt = 10;
 
 @Component({
   selector: 'topbanner',
@@ -10,19 +12,22 @@ let template = require('./topbanner.component.html');
 })
 export class TopBanner implements OnInit, OnDestroy{
   public radioModel:string = 'Homepage';
-  public pageWaiting: number = 120;
+  public pageWaiting: number = waitingCnt;
   ngOnInit(){
     setInterval(()=>{
       if(this.pageWaiting > 0){
         this.pageWaiting -- ;
       }
+      if(this.radioModel != 'Homepage' && this.pageWaiting == 0){
+        this.radioModel= 'Homepage';
+      }
     },1000)
   }
   ngOnDestroy(){
-    this.pageWaiting = 120;
+    this.pageWaiting = waitingCnt;
   }
   resetPageWaiting(){
-    this.pageWaiting = 120;
+    this.pageWaiting = waitingCnt;
   }
 }
 
