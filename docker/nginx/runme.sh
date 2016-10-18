c10f5025e1f05ce95fa64c8abc77a55d11814c93
jeson_peng <jeson.peng@gmail.com>
2016-10-18 09:34:55 +0800

    add docker file

---
+++ docker/nginx/runme.sh
@@ -0,0 +1,6 @@
+docker build -t images-nginx .
+docker run --name images-nginx -d -h images images-nginx
+sudo docker run --name images-nginx -d -v ~/Documents/git/oschina/pics/html:/usr/share/nginx/html -h images --restart unless-stopped images-nginx
+
+docker cp image-nginx:/etc/nginx/nginx.conf ./conf
+docker cp image-nginx:/etc/nginx/conf.d ./conf
